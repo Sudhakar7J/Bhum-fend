79ef9e8504e8da7a642e67600e723b80e1629456
Sudhakar J <35969068+Sudhakar7J@users.noreply.github.com>
2023-07-26 11:44:38 +0530

    Implemented Field Type event to the PDF

--- src/PDFViewer.js
+++ src/PDFViewer.js
@@ -3,11 +3,31 @@ import { PDFDocument } from "pdf-lib"
 import { Document, Page, pdfjs } from "react-pdf"
 import "react-pdf/dist/esm/Page/AnnotationLayer.css"
 import "react-pdf/dist/esm/Page/TextLayer.css"
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.js",
+  import.meta.url
+).toString()
+
 const PDFViewer = () => {
   const [pdfData, setPdfData] = useState(null)
   const [pdfView, setPdfView] = useState(null)
 
+  const [formValues, setFormValues] = useState({})
+
+  const { getInitialFormValues, fetchPDF, updatePDF } = usePDFViewer()
+
+  const initializeFormValues = (fields) => {
+    const initialFormValues = { ...getInitialFormValues(fields) }
+
+    setFormValues({ ...initialFormValues })
+  }
+
   const loadPDF = async () => {
+    const pdfDoc = await fetchPDF()
+
+    initializeFormValues(pdfDoc.getForm().getFields())
+
     const pdfFile = await pdfDoc.save()
 
     setPdfData(pdfFile)
@@ -17,6 +37,16 @@ const PDFViewer = () => {
     })
 
     setPdfView(URL.createObjectURL(modifiedPdfBlob))
+  }
+
+  // Function to set the value of a field based on its type
+  const setFieldValue = (field) => {
+    const newFieldValue = formValues[field.getName()]
+    if (FIELD_TYPE_MAP[field.constructor.name] === "text") {
+      field.setText(newFieldValue)
+    } else {
+      field.select(newFieldValue)
+    }
   }
 
   const savePDF = async () => {
@@ -29,7 +59,42 @@ const PDFViewer = () => {
 
     const formFields = formData.getFields()
 
+    formFields.forEach((field) => {
+      setFieldValue(field)
+    })
+
     const fileToBeUploaded = await pdfDocUpdated.save()
+
+    void updatePDF(fileToBeUploaded)
+  }
+
+  const onTextChange = (event) => {
+    // This is a hacky if block as the value returned by the
+    // onInput event is wrong for this field. It is supposed
+    // to be one of 'parttime' or 'fulltime' but it only returns
+    // 'on'. Also the displayed option and the exported value
+    // is different for the radio group in the second row
+    if (event.target.name.includes("time")) {
+      if (formValues[event.target.name] === "fulltime") {
+        setFormValues((prev) => ({
+          ...prev,
+          [event.target.name]: "parttime",
+        }))
+
+        return
+      }
+
+      setFormValues((prev) => ({
+        ...prev,
+        [event.target.name]: "fulltime",
+      }))
+      return
+    }
+
+    setFormValues((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }))
   }
 
   return (
@@ -41,7 +106,12 @@ const PDFViewer = () => {
       <div>
         {pdfData && (
           <Document file={pdfView}>
-            <Page renderAnnotationLayer renderForms pageNumber={1} />
+            <Page
+              renderAnnotationLayer
+              renderForms
+              pageNumber={1}
+              onInput={onTextChange}
+            />
           </Document>
         )}
       </div>
